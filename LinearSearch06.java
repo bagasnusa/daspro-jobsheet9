0cc2a598dcfb1e8eb309c9bac151b88584e26ce9
bagasnusa <145540024+bagasnusa@users.noreply.github.com>
2023-11-13 19:03:18 +0700

    tugas jobsheet9

---
+++ LinearSearch06.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class LinearSearch06 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        int jumlahElemen = sc.nextInt();
+
+        int[] arrayInt = new int[jumlahElemen];
+
+        
+        for (int i = 0; i < jumlahElemen; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + ": ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        int key = sc.nextInt();
+
+        int hasil = -1; 
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+
+        if (hasil != -1) {
+            System.out.println("Key ada di posisi index ke " + hasil);
+        } else {
+            System.out.println("Key tidak ditemukan.");
+        }
+
+     
+    }
+}
+
